6150e885a77f6e7a607e0407461edb62e7a6ef78
anhtuan <nguyenanhtuan23122000@gmail.com>
2022-09-27 13:30:19 +0700

    add getAllController

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -5,20 +5,31 @@ const getAccountant = async (req, res, next) => {
     try {
         let users = await User.find().populate('roleId', 'name');
         let accountants = []
-        for(let user of users){
-            if(user.roleId){
-                if(user.roleId.name== "accountant"){
+        for (let user of users) {
+            if (user.roleId) {
+                if (user.roleId.name == "accountant") {
                     accountants.push(user)
                 }
             }
         }
         res.status(200).json(accountants)
-        
+
+    } catch (err) {
+        console.log(err);
+        res.status(400).json(err)
+    }
+}
+const getAll = async (req, res, next) => {
+    try {
+        let users = await User.find().populate('roleId', 'name');
+        res.status(200).json(users)
+
     } catch (err) {
         console.log(err);
         res.status(400).json(err)
     }
 }
 module.exports = {
-    getAccountant
+    getAccountant,
+    getAll
 };
--- server/src/routers/userRouter.js
+++ server/src/routers/userRouter.js
@@ -3,5 +3,5 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/accountant',userController.getAccountant)
-
+router.get('',userController.getAll)
 module.exports = router
