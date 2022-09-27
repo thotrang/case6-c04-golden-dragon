e29795087ee857b6918808ad16c79781ecc47d58
anhtuan <nguyenanhtuan23122000@gmail.com>
2022-09-27 09:33:56 +0700

    start

---
+++ server/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv').config();
+
+const PORT = process.env.PORT
+const URL = process.env.MONGO0DB_URL
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const connectDatabase = () => {
+    mongoose.connect(URL)
+    .then((data)=>{
+        console.log('db connect success');
+    }).catch((err)=>{
+        console.log(err);
+    })
+}
+connectDatabase();
+app.listen(PORT, () => {
+    console.log(`http://localhost:${PORT}`)
+})
