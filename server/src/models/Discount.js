7095fbefe8ae87787a7850ee642b1d4d217e8d1c
hung <nhathung3171997@gmail.com>
2022-09-28 09:52:57 +0700

    add model discount

---
+++ server/src/models/Discount.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose') ;
+const {Schema} = require('mongoose');
+
+const DiscountSchema = new Schema({
+    name : {
+        required : true,
+        type : String
+    },
+    productId :[{
+        type : Schema.Types.ObjectId,
+        ref : "Product"
+    }]
+})
+
+module.exports = mongoose.model("Discount",DiscountSchema);
