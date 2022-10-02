e82f8c1530e94a77a5b410ba0e128a755cc06519
sonph <sonph>
2022-10-02 14:05:44 +0700

    fix bug

--- server/src/models/Product.js
+++ server/src/models/Product.js
@@ -1,69 +1,73 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const ProductSchema = new Schema(
+const ProductSchema = new mongoose.Schema(
     {
         name: {
             type: String,
-            required: [true, 'please enter Product name'],
-        },
-        price: {
-            type: String,
-            required: [true, 'please enter Product price'],
+            require: [true, "please enter Product name"],
+            trim: true
         },
         description: {
             type: String,
-            required: [true, 'please enter Product description'],
+            require: [true, "please enter Product description"]
+        },
+        price: {
+            type: Number,
+            require: [true, "please enter Product price"],
+            maxLength: [8, "Price can not longer than 8 characters"]
+        },
+        rating: {
+            type: Number,
+            default: 0
         },
         images: [
             {
                 public_id: {
                     type: String,
-                    require: true,
+                    require: true
                 },
                 url: {
                     type: String,
-                    require: true,
+                    require: true
                 },
-            },
-        ],
-        reviews: [
-            {
-                user: {
-                    type: mongoose.Schema.ObjectId,
-                    ref: "User",
-                    required: true,
-                },
-                name: {
-                    type: String,
-                    required: true,
-                },
-                rating: {
-                    type: Number,
-                    required: true,
-                },
-                comment: {
-                    type: String,
-                    required: true,
-                },
-            },
-        ],
-        rating: {
-            type: Number,
-            default: 0,
-        },
-        categoryId: {
-            type: Schema.Types.ObjectId,
-            ref: 'Category',
-        },
-        brandId: {
-            type: Schema.Types.ObjectId,
-            ref: 'Brand',
-        },
-    },
-    {
-        timestamps: true,
-    },
+            }],
+    }
+    //
+    //     stock:{
+    //         type:Number,
+    //         require:[ true,'Please Enter Product Stock'],
+    //         maxLength: [4,"stock cannot exceed 4 characters"],
+    //         default: 1
+    //     },
+    //     numOfReviews:{
+    //         type:Number,
+    //         default:0
+    //     },
+    //     reviews:[
+    //         {
+    //             name:{
+    //                 type:String,
+    //                 require:true,
+    //             },
+    //             rating:{
+    //                 type:Number,
+    //                 require:true
+    //             },
+    //             comment:{
+    //                 type:String,
+    //                 require:true
+    //             },
+    //
+    //
+    //         brandId: {
+    //         type: Schema.Types.ObjectId,
+    //         ref: 'Brand',
+    //     },
+    // },
+    // {
+    //     timestamps: true,
+    // },
 );
 
 module.exports = mongoose.model('Product', ProductSchema);
