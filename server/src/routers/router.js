1b9751a4c83974a4d0568f80b00297a7c94c6f3d
anhtuan <nguyenanhtuan23122000@gmail.com>
2022-10-06 08:34:55 +0700

    add brandController

--- server/src/routers/router.js
+++ server/src/routers/router.js
@@ -5,10 +5,13 @@ const userRouter = require('./userRouter');
 const roleRouter = require('./roleRouter');
 const authRouter = require('./authRouter');
 const productRouter = require('./productRouter');
+const brandRouter = require('./brandRouter');
 
 router.use('/admin', productRouter);
 
 router.use('', authRouter);
 router.use('/user', userRouter);
 router.use('/role', roleRouter);
+router.use('/brand', brandRouter);
+
 module.exports = router;
