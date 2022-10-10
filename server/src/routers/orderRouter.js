7bf168bc7d093e83e811f2294e7f3f80498ecb52
anhtuan <nguyenanhtuan23122000@gmail.com>
2022-10-11 01:09:54 +0700

    send Order

--- server/src/routers/orderRouter.js
+++ server/src/routers/orderRouter.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const router = express.Router();
 const orderController = require('../controllers/orderController')
-router.get('/',orderController.getAllOrder)
+router.get('/',orderController.getAllOrderPending)
 router.get('/:id',orderController.getDetailOrder)
 router.delete('/:id',orderController.deleteOrder)
 router.put('/:id',orderController.setstatus)
 router.post('',orderController.createOrder)
 router.get('/user/:id',orderController.myOrder)
-
+router.put('/send/:id',orderController.sendOrder)
 module.exports = router;
